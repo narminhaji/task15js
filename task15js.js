d160eabe90b1f645cb4a34455ffd6c0f8a9e7155
narminhaji <haciyevan411@gmail.com>
2024-03-27 22:05:45 +0400

    first commit

---
+++ task15js.js
@@ -0,0 +1,49 @@
+// Daxil olunan ədədin sadə yoxsa mürəkkəb olduğunu tapın.
+let num= +prompt("ededi daxil edin: ")
+for (let i=2;Math.sqrt; i++){
+    if(num%i===0){
+        console.log('${num} bu eded murekkebdir');}
+    else {
+        console.log('${num} bu eded sadedir');
+    }
+}  
+    
+// Daxil olunan ədədin mükəmməl olub olmadığını tapın.
+let number = +prompt(" ededi daxil edin: ");
+  let z= 0;
+ for (let i = 1; i < number; i++) {
+      if ( number% i === 0) {
+          z += i;
+      }
+ }
+ if (s === eded) {
+     console.log('${eded} mukemmeldir');
+ } 
+ else {
+     console.log('${eded} mukemmeldir');
+  }
+ 
+// Daxil olunan 2 ədəd arasındakı sadə ədədləri göstərin.
+let eded = prompt("ededi daxil edin: ")
+let eded1 = prompt("ededi daxil edin: ")
+for (let i=2; i<Math.sqrt; i++){
+        if(eded%i!==0){
+            console.log('${eded} sadedr');}
+        else if(eded1%i!==0)
+        {
+            console.log('${eded1} sadedir');}
+        else{
+            console.log("hec biri")
+        }
+    }  
+
+// Daxil olunan 2 ədəd arasındakı sonu 7 ilə bitən ədədləri göstərin.
+let ed1 = +prompt("1-ci ədədi daxil edin");
+let ed2 = +prompt("2-ci ədədi daxil edin");
+for(let ed=ed1; eded<= ed2;ed++){
+    let data = true;
+    if(ed.toString().endsWith('7')){
+        console.log(ed);
+    }
+}
+
